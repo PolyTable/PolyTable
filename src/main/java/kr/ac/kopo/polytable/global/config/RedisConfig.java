4cf27f25e6e9765408524441dbd7a5807f15f45f
Hounds1 <zoodisx@gmail.com>
2023-02-15 00:38:20 +0900

    redis 설정 추가. -> maria, redis, redis 설정파일 주석처리로 기능하지 않도록 설정.

---
+++ src/main/java/kr/ac/kopo/polytable/global/config/RedisConfig.java
@@ -0,0 +1,56 @@
+package kr.ac.kopo.polytable.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+import static kr.ac.kopo.polytable.global.config.RedisPolicy.*;
+
+
+//@Configuration
+//@EnableCaching
+public class RedisConfig {
+
+//    private final String host;
+//    private final int port;
+//
+//    public RedisConfig(@Value("${spring.redis.host}") String host, @Value("${spring.redis.port}") int port) {
+//        this.host = host;
+//        this.port = port;
+//    }
+//
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        return new LettuceConnectionFactory(host, port);
+//    }
+//
+//    @Bean
+//    public RedisCacheManager redisCacheManager() {
+//        RedisCacheConfiguration configuration = RedisCacheConfiguration
+//                .defaultCacheConfig()
+//                .disableCachingNullValues()
+//                .entryTtl(Duration.ofMinutes(30))
+//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+//                        new StringRedisSerializer()));
+//
+//        Map<String, RedisCacheConfiguration> config = new HashMap<>();
+//        config.put(CERTIFICATION_KEY, RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(CERTIFICATION_TTL)));
+//
+//        return RedisCacheManager.RedisCacheManagerBuilder
+//                .fromConnectionFactory(redisConnectionFactory())
+//                .cacheDefaults(configuration)
+//                .withInitialCacheConfigurations(config)
+//                .build();
+//    }
+}
---
+++ src/main/java/kr/ac/kopo/polytable/global/config/RedisPolicy.java
@@ -0,0 +1,8 @@
+package kr.ac.kopo.polytable.global.config;
+
+public class RedisPolicy {
+
+    public static final String CERTIFICATION_KEY = "C_KEY";
+
+    public static final int CERTIFICATION_TTL = 30;
+}

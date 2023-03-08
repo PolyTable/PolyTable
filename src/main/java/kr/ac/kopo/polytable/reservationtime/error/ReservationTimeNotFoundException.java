179ff7b62ab49cb6da182d565a34a47969e4fea2
hsm <hhpp1205@naver.com>
2023-03-08 13:04:28 +0900

    예약시간 변경 test 예약시간 변경 실패 test 추가

--- src/main/java/kr/ac/kopo/polytable/reservationtime/error/ReservationTimeNotFoundException.java
+++ src/main/java/kr/ac/kopo/polytable/reservationtime/error/ReservationTimeNotFoundException.java
@@ -1,4 +1,7 @@
 package kr.ac.kopo.polytable.reservationtime.error;
 
 public class ReservationTimeNotFoundException extends RuntimeException{
+    public ReservationTimeNotFoundException(String message) {
+        super(message);
+    }
 }
--- src/test/java/kr/ac/kopo/polytable/reservationtime/application/ReservationTimeServiceTest.java
+++ src/test/java/kr/ac/kopo/polytable/reservationtime/application/ReservationTimeServiceTest.java
@@ -1,11 +1,11 @@
 package kr.ac.kopo.polytable.reservationtime.application;
 
+import kr.ac.kopo.polytable.member.error.MemberNotFoundException;
 import kr.ac.kopo.polytable.member.model.Member;
 import kr.ac.kopo.polytable.member.model.repository.MemberRepository;
 import kr.ac.kopo.polytable.reservationtime.dto.ReservationTimeResponse;
 import kr.ac.kopo.polytable.reservationtime.model.ReservationTime;
 import kr.ac.kopo.polytable.reservationtime.model.ReservationTimeRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 @SpringBootTest
 class ReservationTimeServiceTest {
@@ -30,7 +29,10 @@ class ReservationTimeServiceTest {
 
     @Test
     void create_예약시간_생성() {
-        Member value = Member.builder().email("email").username("username").build();
+        Member value = Member.builder()
+                .id(1L)
+                .build();
+
         Optional<Member> member = Optional.of(value);
         given(memberRepository.findById(1L)).willReturn(member);
 
@@ -46,6 +48,23 @@ class ReservationTimeServiceTest {
 
         Long result = reservationTimeService.create(reservationTime, 1L);
         assertThat(result).isEqualTo(1L);
+    }
+
+    @Test
+    void create_예약시간_생성_회원조회_실패() {
+        Optional<Member> member = Optional.empty();
+        given(memberRepository.findById(1L)).willReturn(member);
+
+        ReservationTime reservationTime = ReservationTime.builder()
+                .id(1L)
+                .startTime(LocalTime.of(13, 0, 0))
+                .endTime(LocalTime.of(14, 0, 0))
+                .maximumHeads(30)
+                .build();
+
+        assertThatThrownBy(() -> reservationTimeService.create(reservationTime, 1L))
+                .isInstanceOf(MemberNotFoundException.class)
+                .hasMessage("존재하지 않는 사용자입니다.");
     }
 
     @Test
@@ -89,5 +108,61 @@ class ReservationTimeServiceTest {
                 .containsOnly(rt1.getEndTime(), rt2.getEndTime());
         assertThat(result).extracting("maximumHeads")
                 .containsOnly(rt1.getMaximumHeads(), rt2.getMaximumHeads());
+    }
+
+    @Test
+    void update_예약시간_변경() {
+        Member value = Member.builder()
+                .id(1L)
+                .build();
+
+        Optional<Member> member = Optional.of(value);
+        given(memberRepository.findById(value.getId())).willReturn(member);
+
+        ReservationTime reservationTime = ReservationTime.builder()
+                .id(1L)
+                .startTime(LocalTime.of(13, 0, 0))
+                .endTime(LocalTime.of(14, 0, 0))
+                .maximumHeads(30)
+                .member(member.get())
+                .build();
+
+        given(reservationTimeRepository.findById(reservationTime.getId())).willReturn(Optional.of(reservationTime));
+
+        ReservationTime updateRequest = ReservationTime.builder()
+                .startTime(LocalTime.of(9, 0, 0))
+                .endTime(LocalTime.of(10, 30, 0))
+                .maximumHeads(50)
+                .build();
+
+        reservationTimeService.update(updateRequest, reservationTime.getId(), value.getId());
+
+        assertThat(reservationTime.getStartTime()).isEqualTo(updateRequest.getStartTime());
+        assertThat(reservationTime.getEndTime()).isEqualTo(updateRequest.getEndTime());
+        assertThat(reservationTime.getMaximumHeads()).isEqualTo(updateRequest.getMaximumHeads());
+    }
+
+    @Test
+    void update_예약시간_변경_예약시간_조회_실패() {
+        Member value = Member.builder()
+                .id(1L)
+                .build();
+
+        Optional<Member> member = Optional.of(value);
+        given(memberRepository.findById(value.getId())).willReturn(member);
+
+        ReservationTime reservationTime = ReservationTime.builder()
+                .id(1L)
+                .startTime(LocalTime.of(13, 0, 0))
+                .endTime(LocalTime.of(14, 0, 0))
+                .maximumHeads(30)
+                .member(Member.builder().id(2L).build())
+                .build();
+
+        given(reservationTimeRepository.findById(reservationTime.getId())).willReturn(Optional.of(reservationTime));
+
+        assertThatThrownBy(() -> reservationTimeService.update(reservationTime, reservationTime.getId(), value.getId()))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("사용자의 가게에 속해있는 예약시간이 아닙니다.");
     }
 }
